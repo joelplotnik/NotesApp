43e851ea823b91073e1a8594f1fc2ab126cfcde5
Joel Plotnik <joelplotnik@gmail.com>
2022-10-08 13:45:41 -0400

    Parse arguments with yargs

--- app.js
+++ app.js
@@ -1,7 +1,63 @@
+/*
+    - core modules
+    - npm packages
+    - custom files
+*/
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
-console.log(msg);
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(chalk.blue.inverse.red('Error!'));
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title', // description of argument to be passed
+      demandOption: true, // require an argument to be passed from cl
+      type: 'string', // must be of type string (can be empty)
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing a note!');
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Listing all notes',
+  handler: function () {
+    console.log('Listing out all notes!');
+  },
+});
+
+// Create readd command
+yargs.command({
+  command: 'read',
+  describe: 'Reading the note',
+  handler: function () {
+    console.log('Reading a note!');
+  },
+});
+
+yargs.parse(); // parses the argument with all config details provided
+//console.log(yargs.argv);
