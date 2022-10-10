653d527795efb7d48a3f7efec3792a3086dd9ba4
Joel Plotnik <joelplotnik@gmail.com>
2022-10-10 11:03:39 -0700

    Implement add note functionality

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -27,8 +27,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
